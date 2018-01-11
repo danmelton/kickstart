14703411ca4ed60b2e4f5be70366f2fc95a75e1c
Dan Melton <melton.dan@gmail.com>
2018-01-11 11:27:10 -0800

    hmr working, most of coreui working

--- src/web/index.js
+++ src/web/index.js
@@ -16,14 +16,28 @@ import Page500 from './pages/Pages/Page500/'
 
 // Load css
 
-ReactDOM.render((
-  <HashRouter>
-    <Switch>
-      <Route exact path="/login" name="Login Page" component={Login}/>
-      <Route exact path="/register" name="Register Page" component={Register}/>
-      <Route exact path="/404" name="Page 404" component={Page404}/>
-      <Route exact path="/500" name="Page 500" component={Page500}/>
-      <Route path="/" name="Home" component={Full}/>
-    </Switch>
-  </HashRouter>
-), document.getElementById('root'));
+const render = Component => {
+  ReactDOM.render((
+    <HashRouter>
+      <Switch>
+        <Route exact path="/login" name="Login Page" component={Login}/>
+        <Route exact path="/register" name="Register Page" component={Register}/>
+        <Route exact path="/404" name="Page 404" component={Page404}/>
+        <Route exact path="/500" name="Page 500" component={Page500}/>
+        <Route path="/" name="Home" component={Full}/>
+      </Switch>
+    </HashRouter>
+  ), document.getElementById('root'));
+}
+
+render(Full)
+
+if (module.hot) {
+  module.hot.dispose(function () {
+    render(Full)
+  });
+
+  module.hot.accept(function () {
+    render(Full)
+  });
+}
