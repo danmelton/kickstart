9247225d81a145f24043e424663bf749a3499aa6
Dan Melton <melton.dan@gmail.com>
2018-01-11 12:36:11 -0800

    nearly done, just images left

--- src/web/index.js
+++ src/web/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter, Route, Switch} from 'react-router-dom';
 
-// Temp fix for reactstrap
-// import '../scss/core/_dropdown-menu-right.scss'
-
 // Containers
 import Full from './containers/Full/'
 
